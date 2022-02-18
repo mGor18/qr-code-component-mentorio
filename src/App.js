b0dd64bcad710082da9f92dd41a1a28d25759b09
Matt Gordon <mattg1520@gmail.com>
2022-02-18 02:37:23 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import qrCode from './images/image-qr-code.png';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main id="main-content">
+      <div className="card-container">
+        <div className="qr-img-container">
+          <img src={qrCode} alt="qr scan code" />
+        </div>
+        <section>
+          <div className="qr-info-content">
+            <h1>
+              Improve your front-end skills by building projects
+            </h1>
+            <p>
+              Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
+            </p>
+          </div>
+        </section>
+      </div>
+    </main>
+  )
 }
 
 export default App;
